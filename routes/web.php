75c76db2d3599af153d049156d1293d36557e17b
nttay <nttay@ikornsolutions.com>
2019-04-01 15:55:06 +0700

    Practice route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/abc', function () {
+    return 'welcome abc';
+});
+
+
+Route::get('post', [
+    'as' => 'My Post',
+    function () {
+        return 'welcome post';
+    }
+]);
+
+Route::get('Review routes laravel', function () {
+    return redirect()->route('My Post');
+});
+
+
+Route::get('news', function () {
+    return "welcome news";
+})->name('My News');
+
+Route::get('The weather is cool', function () {
+    return redirect()->route('My News');
+});
+
+
+Route::group(['prefix' => 'group'], function () {
+    Route::get('team1', function () {
+        return "Welcome Team 1";
+    });
+
+    Route::get('team2', function () {
+        return "Welcome Team 2";
+    });
+});
